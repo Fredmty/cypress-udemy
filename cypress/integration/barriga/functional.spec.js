70a3279f9853d2ac184915d3b6948f54beb862fc
Frederico Thofehrn <fredericothofehrn@vipcommerce.com.br>
2022-03-03 17:18:42 -0300

    organizando codigo + criaçao locators

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,21 +1,34 @@
 /// <reference types="cypress" />
 
+import loc from '../../support/locators'
+
 describe('work with alert', () => {
 
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get('[data-test="email"]').type('a@a');
-        cy.get('[data-test="passwd"]').type('a')
-        cy.get('.btn').click();
-        cy.get('.toast-message').should('contain', 'Bem vindo')
+        cy.get(loc.LOGIN.USER).type('a@a');
+        cy.get(loc.LOGIN.PASSWORD).type('a')
+        cy.get(loc.LOGIN.BTN_LOGIN).click();
+        cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
     })
 
     it('deve criar uma conta', () => {
-        cy.get('[data-test="menu-settings"]').click()
-        cy.get('[href="/contas"]').click()
-        cy.get('[data-test="nome"]').type('conta meme')
-        cy.get('.btn').click();
-        cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+        cy.get(loc.MENU.SETTINGS).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.get(loc.CONTAS.NOME).type('conta meme')
+        cy.get(loc.CONTAS.BTN_SALVAR).click();
+        cy.get(LOC.MESSAGE).should('contain', 'Conta inserida com sucesso')
+    });
 
+    it.only('deve alterar uma conta', () => {
+        //cy.get(':nth-child(8) > :nth-child(2) > .fa-edit').click()
+        cy.get(loc.MENU.SETTINGS).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.get(loc.CONTAS.NOME)
+        .clear()
+        .type('conta meme1')
+        cy.get(loc.CONTAS.BTN_SALVAR).click();
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
     });
 })
