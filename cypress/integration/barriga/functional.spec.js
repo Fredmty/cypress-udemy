812b3d1601522dbe3a03ccea3da2811162184483
Frederico Thofehrn <fredericothofehrn@vipcommerce.com.br>
2022-03-04 15:39:28 -0300

    refactor do locators + adicionar e remover transaçoes

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -34,15 +34,38 @@ describe('work with alert', () => {
         cy.get(loc.MESSAGE).should('contain', 'code 400');
     });
 
-    it('deve criar uma transação', () => {
-        cy.get(loc.MENU.MOVIMENTACAO).click();
-        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc');
-        cy.get(loc.MOVIMENTACAO.VALOR).type('123');
-        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('inter');
-        cy.get(loc.MOVIMENTACAO.BTN_SALVAR_MOVIMENTACAO).click();
-        cy.get(loc.MESSAGE).should('contain', 'sucesso');
 
-        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7);
-        cy.get(loc.EXTRATO.BUSCA).should('exist')
-    });
+    it('Deve criar uma transação', () => {
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR_MOVIMENTACAO).click()
+    })
+
+    it('Deve pegar o saldo', () => {
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+        //cy.wait(1000)
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR_MOVIMENTACAO).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.MENU.HOME).click()
+        
+
+    })
+
+    it('Deve remover uma transação', () => {
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+    })
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -1,31 +1,48 @@
 const locators = {
-    LOGIN : {
+    LOGIN: {
         USER: '[data-test=email]',
         PASSWORD: '[data-test=passwd]',
         BTN_LOGIN: '.btn'
     },
-    MENU : {
-        SETTINGS: '[data-test=menu-settings',
+
+    MENU: {
+        HOME: '[data-test=menu-home]',
+        SETTINGS: '.dropdown-toggle',
         CONTAS: '[href="/contas"]',
         RESET: '[href="/reset"]',
-        MOVIMENTACAO: '[data-test=menu-movimentacao]'
+        MOVIMENTACAO: '[data-test=menu-movimentacao]',
+        EXTRATO: '[data-test=menu-extrato]'
     },
-    CONTAS : {
-        NOME: '[data-test=nome]',
+
+    CONTAS: {
+        NOME: '[data-test=nome',
         BTN_SALVAR: '.btn',
         XP_BTN_ALTERAR: "//table//td[contains(., 'conta de teste')]/..//i[@class='far fa-edit']"
     },
+
     MOVIMENTACAO: {
         DESCRICAO: '[data-test=descricao]',
         VALOR: '[data-test=valor]',
         INTERESSADO: '[data-test=envolvido]',
-        BTN_SALVAR_MOVIMENTACAO: '.btn-primary'
+        CONTA: '[data-test=conta]',
+        BTN_SALVAR_MOVIMENTACAO: '.btn-primary',
+        STATUS: '[data-test=status]'
     },
+
     EXTRATO: {
         LINHAS: '.list-group > li',
-        BUSCA: ':nth-child(7) > .row > .col-12 > :nth-child(1) > small'
+        FN_XP_BUSCA_ELEMENTO: (desc, value) => `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${value}')]`,
+        FN_XP_REMOVER_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='far fa-trash-alt']`,
+        FN_XP_ALTERAR_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='fas fa-edit']`,
+        FN_XP_LINHA: desc => `//span[contains(., '${desc}')]/../../../..`
     },
+
+    SALDO: {
+        FN_XP_SALDO_CONTA: nome => `//td[contains(., '${nome}')]/../td[2]`
+    },
+
     MESSAGE: '.toast-message'
+
 }
 
 export default locators;
