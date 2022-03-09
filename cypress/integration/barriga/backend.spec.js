0b9c0c8259b2213eda2ef07e998667ad3033c95f
Frederico Thofehrn <fredericothofehrn@vipcommerce.com.br>
2022-03-09 10:36:32 -0300

    adicionando inserir conta, token e reset

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -1,71 +1,52 @@
 /// <reference types="cypress" />
 
-import loc from '../../support/locators'
-import '../../support/commandsContas'
 
 describe('work with alert', () => {
-
-    before(() => {
-       cy.login('a@a', 'a');
-       cy.resetApp()
+    let token
+        before(() => {
+        cy.getToken('a@a', 'a')
+        .then(tkn => {
+            token = tkn
+        })
+        })
+        beforeEach(() => {
+            cy.resetRest()
+        })
+    
+        it('deve criar uma conta', () => {
+            cy.request({
+                method: 'POST',
+                headers: {Authorization: `JWT ${token}`},
+                url: '/contas',
+                body: { nome: 'conta via rest' },
+            }).as('response')
+           cy.get('@response').then(res => {
+               expect(res.status).to.be.equal(201);
+               expect(res.body).to.have.property('id')
+               expect(res.body).to.have.property('nome', 'conta via rest')
+           })
+        });
+    
+        it('deve alterar uma conta', () => {
+            
+        });
+    
+        it('não deve criar uma conta com o mesmo nome', () => {
+            
+        });
+    
+    
+        it('Deve criar uma transação', () => {
+           
+        })
+    
+        it('Deve pegar o saldo', () => {
+            
+    
+        })
+    
+        it('Deve remover uma transação', () => {
+            
+        })
     })
-
-    it('deve criar uma conta', () => {
-        cy.acessarMenuConta()
-        cy.inserirConta('conta de teste')
-        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
-    });
-
-    it('deve alterar uma conta', () => {
-        //cy.get(':nth-child(8) > :nth-child(2) > .fa-edit').click()
-        cy.acessarMenuConta()
-        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
-        cy.get(loc.CONTAS.NOME)
-        .clear()
-        .type('conta alterada')
-        cy.get(loc.CONTAS.BTN_SALVAR).click();
-        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
-    });
-
-    it('não deve criar uma conta com o mesmo nome', () => {
-        cy.acessarMenuConta()
-        cy.get(loc.CONTAS.NOME).type('Conta para alterar');
-        cy.get(loc.CONTAS.BTN_SALVAR).click();
-        cy.get(loc.MESSAGE).should('contain', 'code 400');
-    });
-
-
-    it('Deve criar uma transação', () => {
-        cy.get(loc.MENU.MOVIMENTACAO).click()
-        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
-        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
-        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
-        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
-        cy.get(loc.MOVIMENTACAO.STATUS).click()
-        cy.get(loc.MOVIMENTACAO.BTN_SALVAR_MOVIMENTACAO).click()
-    })
-
-    it('Deve pegar o saldo', () => {
-        cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
-
-        cy.get(loc.MENU.EXTRATO).click()
-        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
-        //cy.wait(1000)
-        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
-
-        cy.get(loc.MOVIMENTACAO.STATUS).click()
-        cy.get(loc.MOVIMENTACAO.BTN_SALVAR_MOVIMENTACAO).click()
-        cy.get(loc.MESSAGE).should('contain', 'sucesso')
-
-        cy.get(loc.MENU.HOME).click()
-        
-
-    })
-
-    it('Deve remover uma transação', () => {
-        cy.get(loc.MENU.EXTRATO).click()
-        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
-        cy.get(loc.MESSAGE).should('contain', 'sucesso')
-    })
-})
+    
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -44,3 +44,29 @@ Cypress.Commands.add('resetApp', () => {
     cy.get(loc.MENU.SETTINGS).click();
     cy.get(loc.MENU.RESET).click();
 })
+
+Cypress.Commands.add('getToken', (user, passwd) => { 
+    cy.request({
+        method: 'POST',
+        url: '/signin',
+        body: {
+            email: user,
+            redirecionar: false,
+            senha: passwd
+        }
+    }).its('body.token').should('not.be.empty')
+    .then(token => {
+        return token
+    })
+})
+
+Cypress.Commands.add('resetRest', () => { 
+    cy.getToken('a@a', 'a').then(token => {    
+    cy.request({
+        method: 'GET',
+        url: '/reset',
+        headers: { Authorization: `JWT ${token}`}
+
+        }).its('status').should('be.equal', 200)
+    })
+})
